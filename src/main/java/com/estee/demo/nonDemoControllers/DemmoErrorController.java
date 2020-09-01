54a1f06cebd4040c116c926e6ea940d545d382ee
avleichko <mrdezzdemon@gmail.com>
2020-09-01 17:37:30 +0300

    refactoring

--- src/main/java/com/estee/demo/nonDemoControllers/DemmoErrorController.java
+++ src/main/java/com/estee/demo/nonDemoControllers/DemmoErrorController.java
@@ -1,4 +1,4 @@
-package com.estee.demo;
+package com.estee.demo.nonDemoControllers;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
